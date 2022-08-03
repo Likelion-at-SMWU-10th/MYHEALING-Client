9a763967fe6800c6cc2e7570dfdec29e0b0374dc
eunxoo <dmstn7989@naver.com>
2022-08-04 01:23:57 +0900

    [YES] 가이드 작성하기 - 글자수, 사진 첨부

--- myhealing_client/src/page/memories/AddressBtn.js
+++ myhealing_client/src/page/memories/AddressBtn.js
@@ -1,6 +1,6 @@
 import React, { useEffect, useState } from "react";
-import Postcode from '@actbase/react-daum-postcode';
-import Modal from 'react-bootstrap/Modal';
+import Postcode from "@actbase/react-daum-postcode";
+import Modal from "react-bootstrap/Modal";
 
 const AddressBtn = () => {
   const [show, setShow] = useState(false);
@@ -10,32 +10,31 @@ const AddressBtn = () => {
 
   return (
     <>
-        <button variant="primary" onClick={handleShow}>
-            주소 검색
-        </button>
+      <button variant="primary" onClick={handleShow}>
+        주소 검색
+      </button>
 
-        <Modal show={show} onHide={handleClose}>
-            <Modal.Header closeButton>
-            <Modal.Title>주소 검색</Modal.Title>
-            </Modal.Header>
-            <Modal.Body>
-                <Postcode
-                    style={{ width: '100%', height: '100%' }}
-                    jsOptions={{ animation: true, hideMapBtn: true }}
-                    onSelected={data => {
-                        alert(JSON.stringify(data));
-                        setModal(false);
-                  }}
-                />
-            </Modal.Body>
-            <Modal.Footer>
-            <button variant="secondary" onClick={handleClose}>
-                닫기
-            </button>
-            </Modal.Footer>
-        </Modal>
+      <Modal show={show} onHide={handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>주소 검색</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <Postcode
+            style={{ width: "100%", height: "100%" }}
+            jsOptions={{ animation: true, hideMapBtn: true }}
+            onSelected={(data) => {
+              alert(JSON.stringify(data));
+            }}
+          />
+        </Modal.Body>
+        <Modal.Footer>
+          <button variant="secondary" onClick={handleClose}>
+            닫기
+          </button>
+        </Modal.Footer>
+      </Modal>
     </>
   );
-}
+};
 
 export default AddressBtn;
---
+++ myhealing_client/src/page/memories/UploadPhoto.js
@@ -0,0 +1,174 @@
+import React, { useState } from "react";
+import styled from "styled-components";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/swiper.min.css";
+import "swiper/components/zoom/zoom.min.css";
+import "swiper/components/navigation/navigation.min.css";
+import "swiper/components/pagination/pagination.min.css";
+import SwiperCore, { Navigation, Pagination } from "swiper";
+
+import { GrGallery } from "react-icons/gr";
+
+SwiperCore.use([Navigation, Pagination]);
+
+const UploadPhoto = () => {
+  //사진 첨부
+  const [img, setImg] = useState([]);
+  const addImg = (e) => {
+    const nowSelectImgList = e.target.files;
+    const nowImgUrlList = [...img];
+    for (let i = 0; i < nowSelectImgList.length; i += 1) {
+      const nowImgUrl = URL.createObjectURL(nowSelectImgList[i]);
+      nowImgUrlList.push(nowImgUrl);
+    }
+
+    setImg(nowImgUrlList);
+    e.target.value = "";
+  };
+
+  const deleteImg = (e) => {
+    const nowIdx = e.target.value;
+    const copyMyImg = [...img];
+
+    copyMyImg.splice(nowIdx, 1);
+
+    setImg(copyMyImg);
+  };
+
+  return (
+    <>
+      <Wrapper>
+        {img.length === 0 ? (
+          <NoImg>사진을 추가해주세요</NoImg>
+        ) : (
+          <SwiperImg
+            slidesPerView={1}
+            navigation
+            pagination={{ clickable: true }}
+          >
+            {img.map(function imageList(image, i) {
+              return (
+                <SwiperSlide key={image}>
+                  <ImgContainer>
+                    <ImgDiv>
+                      <Imgs src={image} alt="사진을 추가해주세요" />
+                    </ImgDiv>
+                    <DeleteBtn type="submit" onClick={deleteImg} value={i}>
+                      X
+                    </DeleteBtn>
+                  </ImgContainer>
+                </SwiperSlide>
+              );
+            })}
+          </SwiperImg>
+        )}
+      </Wrapper>
+      <div>
+        <AddLabel htmlFor="input-file" onChange={addImg}>
+          <GrGallery />
+          <Ptag>사진 첨부</Ptag>
+          <input
+            type="file"
+            multiple
+            id="input-file"
+            style={{ display: "none", width: "100%", height: "100%" }}
+            accept="image/*"
+          />
+        </AddLabel>
+      </div>
+    </>
+  );
+};
+
+const Wrapper = styled.div`
+  & .swiper-button-prev {
+    color: #73bd88;
+  }
+
+  & .swiper-button-next {
+    color: #73bd88;
+  }
+  & .swiper-pagination {
+  }
+
+  & .swiper-pagination-bullet {
+    background-color: #73bd88;
+    margin: 0 5px !important;
+  }
+`;
+
+const NoImg = styled.div`
+  font-size: 15px;
+  width: 100%;
+  height: 20vh;
+  margin-left: auto;
+  margin-right: auto;
+  margin-top: 2rem;
+  margin-bottom: 1rem;
+  border: 1px dashed lightgray;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  color: lightgrey;
+  text-align: center;
+`;
+
+const SwiperImg = styled(Swiper)`
+  width: auto;
+  height: 14rem;
+`;
+
+const ImgContainer = styled.div`
+  display: flex;
+  flex-direction: row;
+  justify-content: center;
+  width: 100%;
+  height: 100%;
+  text-align: center;
+`;
+
+const ImgDiv = styled.div`
+  width: auto;
+  height: 85%;
+`;
+const Imgs = styled.img`
+  width: 100%;
+  height: 100%;
+`;
+
+const DeleteBtn = styled.button`
+  color: #73bd88;
+  width: 20px;
+  height: 20px;
+  font-size: 15px;
+  font-weight: 1000;
+  border: none;
+  background-color: #ffffff;
+  text-align: center;
+`;
+
+const AddLabel = styled.label`
+  margin-top: 10px;
+  padding: 6px 15px;
+  color: black;
+  cursor: pointer;
+  margin-bottom: 30px;
+
+  &:hover {
+    padding-left: 1px;
+  }
+
+  &:active {
+    padding-left: 1px;
+  }
+`;
+
+const Ptag = styled.p`
+  float: right;
+  margin-left: 10px;
+  margin-top: 2px;
+  color: #73bd88;
+`;
+
+export default UploadPhoto;
