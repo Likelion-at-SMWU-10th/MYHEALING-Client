0eab535ad137dbe84ce5b32523bfc0b9c8f2b1b3
sumin <tnals8939@daum.net>
2022-08-13 17:53:59 +0900

    [KSM] 자체 로그인 및 회원가입 서버 연동 완료
    [KSM] 자체 로그인 및 회원가입 서버 연동 완료

--- myhealing_client/src/page/login/OurLogin.js
+++ myhealing_client/src/page/login/OurLogin.js
@@ -1,19 +1,90 @@
 import React from 'react';
 import "./OurLogin.css";
+import { useState, useRef } from 'react';
+import { useNavigate } from 'react-router-dom';
+import axios from "axios";
 
 const OurLogin = () => {
+    const [values, setValues] = useState({ 
+        user_id: "",
+        password: ""
+    });
+
+    const navigate = useNavigate();
+    const handleChange = (event) => {
+        const { name, value } = event.target;
+        setValues({ ...values, [name]: value });
+    };
+
+    const setPopup = (values) => {
+        alert( values.message);
+        navigate('/login');
+    }
+
+    const submitSignUp = () =>
+    {
+        axios.post("http://127.0.0.1:8000/login/", {
+            user_id: values.user_id,
+            password: values.password
+        }).then(function (response) {
+            console.log('response : '+response);
+            if(response.data.code == 0){
+                setPopup({
+                    open: true,
+                    title: "Confirm",
+                    message: "Login Success!"
+                });
+            } else {
+                console.log(response)
+                let message = response.data.message;
+                if(response.data.code == 10000){
+                    message = "User ID is duplicated. Please enter a different User ID. "
+                }
+                setPopup({
+                    open: true,
+                    title: "Error",
+                    message: message
+                });
+            }
+        }).catch(function (error) {
+            alert('로그인에 실패했습니다.');
+            console.log(error);
+        });
+    }
+
     return (
         <div>
             <form>
                 <div className="mb-3">
                     <label className="form-label">ID</label>
-                    <input type="id" className="form-control loginId" id="exampleInputId1" aria-describedby="idHelp"/>
+                    <input 
+                        type="id" 
+                        className="form-control loginId"
+                        id="user_id"
+                        name="user_id"
+                        aria-describedby="idHelp"
+                        
+                        value={values.user_id}
+                        onChange={handleChange}/>
                 </div>
                 <div className="mb-3">
                     <label className="form-label">PASSWORD</label>
-                    <input type="password" className="form-control loginId" id="exampleInputPassword1"/>
+                    <input 
+                        type="password" 
+                        className="form-control loginId" 
+                        id="password"
+                        name="password"
+                        
+                        value={values.password}
+                        onChange={handleChange}/>
                 </div>
-                <button type="submit" className="btn btn-primary ourLoginBtn">LOGIN</button>
+                <button 
+                    type="button" 
+                    className="btn btn-primary ourLoginBtn"
+                    onClick={() => submitSignUp()}
+                >
+                    LOGIN
+                </button>
             </form>
             <br/>
         </div>
